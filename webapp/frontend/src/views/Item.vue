82d0a823ab502e852efe0be40088f99c566d23fa
nicklzx <48320374+inekit@users.noreply.github.com>
2023-03-03 18:19:11 +0300

    ADD favorites

--- webapp/frontend/src/views/Item.vue
+++ webapp/frontend/src/views/Item.vue
@@ -35,7 +35,6 @@ import 'vue3-carousel/dist/carousel.css'
 import { Carousel, Slide, Pagination, Navigation } from 'vue3-carousel'
 import moment from "moment"
 import { ListLoader, InstagramLoader } from 'vue-content-loader'
-import toggleFavorite from '@/utils/toggleFavorite'
 export default {
     components: {
         InstagramLoader,
@@ -119,8 +118,44 @@ export default {
             return moment(date).format("DD.MM.YYYY")
         },
         toggleFav(event, item) {
-            toggleFavorite(this, event, item)
-        },
+            const isFavorite = item.is_favorite
+
+            if (isFavorite) {
+                this.$store.state.myApi
+                    .delete(this.$store.state.restAddr + '/favorites', {
+                        data: {
+                            item_id: item.id,
+                            user_id: this.$store.state.user_id,
+                        },
+                    })
+                    .then((response) => {
+                        item.is_favorite = false
+                        if (this.$route.name === 'Favorites') {
+                            console.log(1212)
+                            this.$store.state.results = this.$store.state.results?.filter(
+                                (el) => el.id !== item.id,
+                            )
+                            if (this.$store.state.results === 0)
+                                window.Telegram?.WebApp.MainButton.hide()
+                        }
+                    })
+                    .catch((e) => {
+                        eventBus.$emit('noresponse', e)
+                    })
+            } else {
+                this.$store.state.myApi
+                    .put(this.$store.state.restAddr + '/favorites', {
+                        item_id: item.id,
+                        user_id: this.$store.state.user_id,
+                    })
+                    .then((response) => {
+                        item.is_favorite = true
+                    })
+                    .catch((e) => {
+                        eventBus.$emit('noresponse', e)
+                    })
+            }
+        }
 
     }
 }
