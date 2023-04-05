1642fb3a6255bf75743a3f7f946be574a67042c8
nicklzx <48320374+inekit@users.noreply.github.com>
2023-04-05 19:49:35 +0300

    first commit

--- webapp/backend/src/server/passport/passport.js
+++ webapp/backend/src/server/passport/passport.js
@@ -1,18 +1,18 @@
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const bcrypt = require('bcryptjs');
-const tOrmCon = require('../db/connection');
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcryptjs");
+const tOrmCon = require("../../db/connection");
 
 passport.serializeUser(function (user, done) {
-  console.log('Сериализация: ', user.id);
+  console.log("Сериализация: ", user.id);
   done(null, user.id);
 });
 
 passport.deserializeUser(function (id, done) {
-  console.log('Десериализация: ', id);
+  console.log("Десериализация: ", id);
 
   tOrmCon.then((connection) => {
-    const userRep = connection.getRepository('Admin');
+    const userRep = connection.getRepository("Admin");
 
     userRep
       .findOne({ where: { id: id } })
@@ -28,30 +28,37 @@ passport.deserializeUser(function (id, done) {
 });
 
 passport.use(
-  new LocalStrategy({ usernameField: 'login' }, function (login, password, done) {
+  new LocalStrategy({ usernameField: "login" }, function (
+    login,
+    password,
+    done
+  ) {
     tOrmCon.then((connection) => {
-      const userRep = connection.getRepository('Admin');
+      const userRep = connection.getRepository("Admin");
 
       userRep
         .find()
         .then(function (allUsers) {
-          console.log('All users: ', allUsers);
+          console.log("All users: ", allUsers);
 
           if (!allUsers) return done(error, false);
 
           for (const userN of allUsers) {
-            if (login === userN.login && bcrypt.compareSync(password, userN.password)) {
-              console.log('логин правильный, вы ', userN.id);
+            if (
+              login === userN.login &&
+              bcrypt.compareSync(password, userN.password)
+            ) {
+              console.log("логин правильный, вы ", userN.id);
 
               return done(null, userN);
             }
           }
 
-          console.log('логин неправильный');
+          console.log("логин неправильный");
           return done(null, false);
         })
         .catch(function (error) {
-          console.log('Ошибка доступа', error);
+          console.log("Ошибка доступа", error);
           done(null, false);
         });
     });
