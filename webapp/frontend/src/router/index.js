cb2a9411c47eabec014537d0196a17a3136a7bbc
nicklzx <48320374+inekit@users.noreply.github.com>
2023-04-05 17:04:32 +0300

    first commit

--- webapp/frontend/src/router/index.js
+++ webapp/frontend/src/router/index.js
@@ -47,7 +47,7 @@ const routes = [
 
 const router = createRouter({
   //history: createWebHashHistory(process.env.BASE_URL),
-  history: createWebHistory(),
+  history: createWebHistory('/colorsfront/'),
   routes,
   scrollBehavior() {
     // always scroll to top
--- webapp/frontend/vue.config.js
+++ webapp/frontend/vue.config.js
@@ -1,5 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   publicPath: '/colorsfront/',
+  devServer: {
+    https: false,
+    public: 'localhost:80',
+    host: '0.0.0.0',
+    port: 80,
+  },
   transpileDependencies: true,
 })
