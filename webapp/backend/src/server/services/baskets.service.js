7cd47fec0939c7e0eb4ffca71d473b6890dd7487
nicklzx <48320374+inekit@users.noreply.github.com>
2023-04-13 13:40:52 +0300

    design issues

--- webapp/backend/src/server/services/baskets.service.js
+++ webapp/backend/src/server/services/baskets.service.js
@@ -171,7 +171,7 @@ class BasketsService {
 
       connection
         .query(
-          `select u.name, u.surname, u.patronymic, u.address, u.phone 
+          `select u.name, u.surname, u.patronymic, u.address, u.phone, sum(io.price) total 
           from users u 
           left join orders o on u.id = o.user_id
           left join order_items oi on oi.order_id = o.id
