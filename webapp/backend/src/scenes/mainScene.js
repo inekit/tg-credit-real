638bf9aa354b6d4c1173827618243c75e7830159
nicklzx <48320374+inekit@users.noreply.github.com>
2023-04-13 15:51:42 +0300

    design issues

--- webapp/backend/src/scenes/mainScene.js
+++ webapp/backend/src/scenes/mainScene.js
@@ -35,7 +35,10 @@ const scene = new CustomWizardScene("clientScene").enter(async (ctx) => {
         ctx.replyWithTitle("DB_ERROR");
       });
   }
-
+  ctx.replyWithKeyboard("WEBAPP_TITLE", {
+    name: "webapp_keyboard",
+    args: [ctx.from.id],
+  });
   ctx.replyWithKeyboard("START_TITLE", {
     name: "main_menu_keyboard",
   });
