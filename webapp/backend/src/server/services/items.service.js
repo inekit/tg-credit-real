ec3a46433c72cdc4bc921937a5b69a5a05531efe
nicklzx <48320374+inekit@users.noreply.github.com>
2023-04-19 00:34:01 +0300

    design issues

--- webapp/backend/src/server/services/items.service.js
+++ webapp/backend/src/server/services/items.service.js
@@ -78,7 +78,7 @@ class UsersService {
               where oi2.item_option_id = $1
               and o2.user_id = $2
               and o2.status = 'basket'
-              group by p.id, io.id
+              group by p.id, io.id,io.size,io.material,io.price
               limit 1`,
             [backside_id, user_id]
           )
