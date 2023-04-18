90ee4a993f0424f3880ddbc7b543cbfc1d8bcdf9
nicklzx <48320374+inekit@users.noreply.github.com>
2023-04-18 23:16:48 +0300

    design issues

--- webapp/backend/src/server/services/items.service.js
+++ webapp/backend/src/server/services/items.service.js
@@ -95,7 +95,7 @@ class UsersService {
                 group by p.id
                 order by ${orderQueryPart}
                 LIMIT $4 OFFSET $5`,
-            [searchQuery, category, id, take, skip, user_id]
+            [searchQuery, category, id, take, skip]
           )
           .then((data) => res(data))
           .catch((error) => rej(new MySqlError(error)));
