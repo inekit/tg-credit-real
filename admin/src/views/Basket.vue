5bbb530a99b9dbae00720cbf2760d1b942281b0f
nicklzx <48320374+inekit@users.noreply.github.com>
2023-04-26 22:01:13 +0300

    fr

--- admin/src/views/Basket.vue
+++ admin/src/views/Basket.vue
@@ -49,7 +49,7 @@ export default {
                     title: 'Размер',
                 },
                 {
-                    name: 'size',
+                    name: 'material',
                     title: 'Материал',
                 },
                 {
--- admin/src/views/Order.vue
+++ admin/src/views/Order.vue
@@ -86,7 +86,7 @@ export default {
                     title: 'Размер',
                 },
                 {
-                    name: 'size',
+                    name: 'material',
                     title: 'Материал',
                 },
                 {
