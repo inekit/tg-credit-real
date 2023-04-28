3a10745208f768d7ad9f718ca1684e750ced80f2
nicklzx <48320374+inekit@users.noreply.github.com>
2023-04-28 12:34:56 +0300

    fr

--- webapp/backend/src/server/services/baskets.service.js
+++ webapp/backend/src/server/services/baskets.service.js
@@ -205,7 +205,7 @@ class BasketsService {
 
       connection
         .query(
-          `select u.name, u.surname, u.patronymic, u.address, u.phone, sum(io.price * oi.count)::int+individual_price  total, 
+          `select u.name, u.surname, u.patronymic, u.address, u.phone, sum(io.price * oi.count)::int+individual_price::int  total, 
           individual_text, individual_price,
           sum(oi.count)::int  total_count
           from users u 
