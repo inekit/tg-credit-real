5b99e24b58b1ab7b245bf532160f09e3916982c3
nicklzx <48320374+inekit@users.noreply.github.com>
2023-04-28 16:06:43 +0300

    fr

--- admin/src/views/Basket.vue
+++ admin/src/views/Basket.vue
@@ -181,9 +181,9 @@ export default {
                         if (el.mainside_id) el.title = `${el.title} (обр - ${el.mainside_id})`;
                         return el
                     })?.sort((a, b) => a.mainside_id ? 1 : b.mainside_id ? -1 : 0)
-                        ?.sort((a, b) => a.mainside_id ? a.mainside_id !== b.id ? -1 : 1
-                            : b.mainside_id ? b.mainside_id !== a.id ? 1 : -1
-                                : 0)
+                        ?.sort((a, b) => b.mainside_id ?
+                            b.mainside_id !== a.id ? 1 : -1
+                            : 0)
 
                 })
                 .catch(() => {
