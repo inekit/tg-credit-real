dd9868e91b542fd8b0722127f6c11aeae785c6c9
nicklzx <48320374+inekit@users.noreply.github.com>
2023-04-29 20:47:27 +0300

    fr

--- admin/src/components/AddStaticModal.vue
+++ admin/src/components/AddStaticModal.vue
@@ -7,8 +7,8 @@
       <form ref="add-file-form" novalidate :validated="formValid" @submit.prevent="addTag" class="add-user"
         style="display: 'none'">
         <span>Приветствие</span>
-        <QuillEditor theme="snow" toolbar="essential" ref="greetingEditor" id="greetingEditor"
-          placeholder="Текст приветствия" class="mb-3" />
+        <QuillEditor theme="snow" :toolbar="['bold', 'italic', 'underline', 'strike']" ref="greetingEditor"
+          id="greetingEditor" placeholder="Текст приветствия" class="mb-3" />
         <span>Корзина</span>
         <QuillEditor theme="snow" toolbar="essential" ref="orderEditor" id="orderEditor" placeholder="Текст в корзине" />
       </form>
