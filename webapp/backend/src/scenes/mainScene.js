ef21b626ef78c5fd83a1c812a488c338bbf036b7
nicklzx <48320374+inekit@users.noreply.github.com>
2023-04-29 20:34:28 +0300

    fr

--- webapp/backend/src/scenes/mainScene.js
+++ webapp/backend/src/scenes/mainScene.js
@@ -43,7 +43,7 @@ const scene = new CustomWizardScene("clientScene").enter(async (ctx) => {
   const clearedGreeting = static.greeting
     ?.replaceAll("<p>", "")
     ?.replaceAll("</p>", "\n")
-    ?.replaceAll(/\<\/?(br|ol|li|ul|blockquote)(\/)?\>/g, "");
+    ?.replaceAll(/\<\/?(br|ol|li|ul|blockquote|span)(.+)?\>/g, "");
 
   ctx.replyWithKeyboard(clearedGreeting, "main_menu_keyboard");
   //"START_TITLE"
