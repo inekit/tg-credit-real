a3a063cd3354418b0cc7c54e6d14380a9f0a6116
nicklzx <48320374+inekit@users.noreply.github.com>
2023-05-03 02:21:34 +0300

    initial commit

--- webapp/frontend/src/views/Item.vue
+++ webapp/frontend/src/views/Item.vue
@@ -6,7 +6,7 @@
     <div class="img-container">
         <carousel>
             <slide class="carousel__item"
-                v-for="img_link, img_id in item.options_array?.find(el => el.id === selected_option)" :key="img_id">
+                v-for="img_link, img_id in item.options_array?.find(el => el.id === selected_option)?.photos" :key="img_id">
                 <img v-if="['webp', 'jpg', 'jpeg', 'png'].includes(img_link?.split('.')?.reverse()[0]?.toLowerCase())"
                     :src="`/colorsserver/public/pics/${img_link}`" />
                 <video style="max-height: 100%;max-width: 100%;" v-else :src="`/colorsserver/public/pics/${img_link}`"
