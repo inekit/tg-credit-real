72eb649647493d25c150233aa28ae55290b95bd4
nicklzx <48320374+inekit@users.noreply.github.com>
2023-05-05 14:24:04 +0300

    initial commit

--- webapp/frontend/src/views/Order.vue
+++ webapp/frontend/src/views/Order.vue
@@ -17,7 +17,7 @@
         <div class="delivery">
             <h2>Способ доставки</h2>
             <div class="select-group">
-                <div v-for="dm in                          deliveryMethods                         " :key="dm">
+                <div v-for="dm in                              deliveryMethods                             " :key="dm">
                     <input type="radio" :id="dm" :value="dm" v-model="selected_dm" @change="getDeliveryPrice">
                     <label :for="dm" @click="selected_dm = dm; getDeliveryPrice()">{{ dm }}</label>
                 </div>
@@ -26,7 +26,7 @@
         <div class="payment">
             <h2>Способ оплаты</h2>
             <div class="select-group">
-                <div v-for="                       po                        in                        paymentOptions                         "
+                <div v-for="                           po                            in                            paymentOptions                             "
                     :key=" po ">
                     <input type="radio" :id=" po " :value=" po " v-model=" selected_po ">
                     <label :for=" po " @click=" selected_po = po ">{{ po }}</label>
@@ -145,22 +145,23 @@ export default {
         async getDeliveryPrice() {
             console.log(this.selected_dm === "CДЭК")
             if (!this.selected_dm === "CДЭК") return this.deliveryPrice = null;
-            const results = await this.$store.state.myApi.get(this.$store.state.restAddr + '/delivery_price', {
-                params: {
-                    operator: this.selected_dm,
-                    address: this.basketData.address,
-                    count: this.basketData.total_count,
-                    postal_code: this.basketData.postal_code,
-                }
-            })
-                .then(response => {
-                    this.deliveryPrice = response.data?.price ?? null;
-                    this.deliveryTime = response.data?.time ?? null;
+            else {
+                const results = await this.$store.state.myApi.get(this.$store.state.restAddr + '/delivery_price', {
+                    params: {
+                        operator: this.selected_dm,
+                        address: this.basketData.address,
+                        count: this.basketData.total_count,
+                        postal_code: this.basketData.postal_code,
+                    }
                 })
-                .catch(e => { console.log(e); this.deliveryPrice = null; this.deliveryTime = null })
+                    .then(response => {
+                        this.deliveryPrice = response.data?.price ?? null;
+                        this.deliveryTime = response.data?.time ?? null;
+                    })
+                    .catch(e => { console.log(e); this.deliveryPrice = null; this.deliveryTime = null })
 
-            return results ?? {}
-
+                return results ?? {}
+            }
         },
         getDeliveryPricePostal() {
             if ([0, 6].includes(this.basketData.postal_code?.toString()?.length ?? 0)) this.getDeliveryPrice()
