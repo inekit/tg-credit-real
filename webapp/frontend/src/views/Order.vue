f516b62749d47a1ba3b6743c3717bd566c8afefa
nicklzx <48320374+inekit@users.noreply.github.com>
2023-05-05 14:18:48 +0300

    initial commit

--- webapp/frontend/src/views/Order.vue
+++ webapp/frontend/src/views/Order.vue
@@ -17,7 +17,7 @@
         <div class="delivery">
             <h2>Способ доставки</h2>
             <div class="select-group">
-                <div v-for="dm in               deliveryMethods              " :key="dm">
+                <div v-for="dm in                    deliveryMethods                   " :key="dm">
                     <input type="radio" :id="dm" :value="dm" v-model="selected_dm" @change="getDeliveryPrice">
                     <label :for="dm" @click="selected_dm = dm; getDeliveryPrice()">{{ dm }}</label>
                 </div>
@@ -26,7 +26,8 @@
         <div class="payment">
             <h2>Способ оплаты</h2>
             <div class="select-group">
-                <div v-for="            po             in             paymentOptions              " :key=" po ">
+                <div v-for="                 po                  in                  paymentOptions                   "
+                    :key=" po ">
                     <input type="radio" :id=" po " :value=" po " v-model=" selected_po ">
                     <label :for=" po " @click=" selected_po = po ">{{ po }}</label>
                 </div>
@@ -161,6 +162,8 @@ export default {
 
         },
         getDeliveryPricePostal() {
+            if (!this.selected_dm === "CДЭК") return this.deliveryPrice = null;
+
             if ([0, 6].includes(this.basketData.postal_code?.toString()?.length ?? 0)) this.getDeliveryPrice()
             else { this.deliveryPrice = null; this.deliveryTime = null }
         },
