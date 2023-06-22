1977587fc894dbc0e1d2d4e0aa24652a869d37ca
nicklzx <48320374+inekit@users.noreply.github.com>
2023-06-22 19:10:05 +0300

    add opt sales

--- webapp/backend/src/db/entity/Item.js
+++ webapp/backend/src/db/entity/Item.js
@@ -32,8 +32,8 @@ module.exports = new EntitySchema({
       nullable: false,
       default: 100,
     },
-    saleCount: { type: "int", nullable: true },
-    salePrice: { type: "int", nullable: true },
+    sale_count: { type: "int", nullable: true },
+    sale_price: { type: "int", nullable: true },
     preview: {
       type: "varchar",
       length: 300,
