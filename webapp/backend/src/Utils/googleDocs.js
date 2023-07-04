2875b30b6158dc45615a5d5f71491287308d1d4f
nicklzx <48320374+inekit@users.noreply.github.com>
2023-07-04 20:20:06 +0300

    add opt sales

--- webapp/backend/src/Utils/googleDocs.js
+++ webapp/backend/src/Utils/googleDocs.js
@@ -142,9 +142,35 @@ async function addOrder(orderData = {}) {
       },
     ],
   };
+
   res = await sheets.spreadsheets.batchUpdate({
     spreadsheetId,
     resource: batchUpdateRequest,
+  });
+
+  console.log(res.data);
+
+  res = await sheets.spreadsheets.batchUpdate({
+    spreadsheetId,
+    resource: {
+      requests: [
+        {
+          updateDimensionGroup: {
+            dimensionGroup: {
+              range: {
+                sheetId: 1018969262,
+                dimension: "ROWS",
+                startIndex: 1,
+                endIndex: lastIdRow,
+              },
+              collapsed: true,
+              depth: 1,
+            },
+            fields: "collapsed",
+          },
+        },
+      ],
+    },
   });
 
   console.log(res.data);
