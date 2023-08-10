5d277d63c1948d10fed44300da21d69ae0472d57
nicklzx <48320374+inekit@users.noreply.github.com>
2023-08-10 13:00:49 +0300

    add stolbtsi

--- webapp/backend/src/server/services/orders.service.js
+++ webapp/backend/src/server/services/orders.service.js
@@ -75,7 +75,8 @@ class UsersService {
           json_agg(DISTINCT jsonb_build_object('title', i.title,'count',oi.count, 
            'option_id', io.id, 'option_name', io.name, 'price', i.price)) items,
            o.surname||' '|| o.name || ' ' || o.patronymic as fio,
-          count (uo.id) orders_count,
+          count (CASE WHEN uo.status <> 'basket' then 1 ELSE NULL END) orders_count,
+          json_agg(DISTINCT jsonb_build_object('id',uo.id)) orderss
           u.username
           from orders o 
           left join order_items oi on o.id = oi.order_id  
