8b8484023e6f287f7a71461042ee0d52728cd2d9
nicklzx <48320374+inekit@users.noreply.github.com>
2023-08-17 15:59:57 +0300

    stat

--- webapp/frontend/src/components/Navbar.vue
+++ webapp/frontend/src/components/Navbar.vue
@@ -2,8 +2,9 @@
     <div class="BottomNavBar_bottomNavBar">
         <div class="container">
             <div class="BottomNavBar_bottomNavBar__body">
-                <RouterLink aria-current="page" class="_icon-home BottomNavBar_bottomNavBar__link" active-class="active"
-                    :to="`/channels/${$store.state.userId}`"></RouterLink>
+                <RouterLink aria-current="page" class="_icon-home BottomNavBar_bottomNavBar__link"
+                    :class="[$route.name !== Favourites ? 'active' : '']" :to="`/channels/${$store.state.userId}`">
+                </RouterLink>
                 <RouterLink class="_icon-bookmark BottomNavBar_bottomNavBar__link" active-class="active"
                     :to="'/favourites'"></RouterLink>
             </div>
