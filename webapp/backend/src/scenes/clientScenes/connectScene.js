372fbe4c3ee33bf611bef53ad733e5c639b3da84
nicklzx <48320374+inekit@users.noreply.github.com>
2023-08-22 19:07:42 +0300

    stat

--- webapp/backend/src/scenes/clientScenes/connectScene.js
+++ webapp/backend/src/scenes/clientScenes/connectScene.js
@@ -36,12 +36,12 @@ scene
       3: "add_type_3",
       4: "add_type_ALL",
     },
-    handler: new Composer().action(/^add\_type\_(.+)$/g, async (ctx) => {
+    cb: async (ctx) => {
       await ctx.answerCbQuery().catch(console.log);
       ctx.scene.state.input.add_type = ctx.match[1];
       console.log(ctx.match);
       ctx.replyNextStep();
-    }),
+    },
   })
   .addStep({
     variable: "post",
