ed271734ce731f4dad004bd3239609beff9f08ab
nicklzx <48320374+inekit@users.noreply.github.com>
2023-09-05 22:34:44 +0300

    first commit

--- webapp/backend/src/server/services/loans.service.js
+++ webapp/backend/src/server/services/loans.service.js
@@ -52,7 +52,7 @@ class BasketsService {
       const connection = await tOrmCon;
       {
         const query = `select l.*, u.username,
-        o.surname||' '|| o.name || ' ' || o.patronymic as fio
+        surname||' '|| name || ' ' || patronymic as fio
                     from public.loans l
                     left join users u on u.id = l.user_id
                     where (lower(name) like lower($1) or lower(surname) like lower($1) or lower(patronymic) like lower($1) or $1 is NULL)
