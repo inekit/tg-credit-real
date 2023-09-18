49dd86387f8e53e1365080279c0fc8646f3d4013
nicklzx <48320374+inekit@users.noreply.github.com>
2023-09-18 18:34:16 +0300

    admin fixes

--- admin/src/utils/dateFormatter.js
+++ admin/src/utils/dateFormatter.js
@@ -2,13 +2,15 @@ let options = {
   year: 'numeric',
   month: 'long',
   day: 'numeric',
-};
+}
 
 module.exports = {
   dateFormatter: (date) => {
-    return new Date(date).toLocaleDateString('ru-RU', options);
+    return date ? new Date(date).toLocaleDateString('ru-RU', options) : ''
   },
   getAgeStr(months) {
-    return months > 12 ? `${Math.trunc(months / 12)} лет ${months % 12} месяцев` : `${months} месяцев`;
+    return months > 12
+      ? `${Math.trunc(months / 12)} лет ${months % 12} месяцев`
+      : `${months} месяцев`
   },
-};
+}
